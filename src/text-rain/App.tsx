31d189f52eb4e49a00ba6c9fdae03f52fcaa2280
sanjee <sanjee.yogeswaran@gmail.com>
2026-04-02 10:16:52 -0500

    Load matrix layout styles in shared app

--- src/text-rain/App.tsx
+++ src/text-rain/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { OrbitControls } from '@react-three/drei'
+import '../index.css'
 import MatrixRain from './MatrixRain'
 import MatrixEffects from './MatrixEffects'
 import {
